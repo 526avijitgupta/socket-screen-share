b4b788ca7088078a1eabf5100a95ef0e1403be82
Avijit Gupta <526avijit@gmail.com>
2016-04-04 16:00:29 +0530

    Update .gitignore

--- server/modules/create_socket.py
+++ server/modules/create_socket.py
@@ -2,6 +2,7 @@ import socket
 
 def start_server(host, port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.bind((host, port))
     s.listen(5)
     return s
