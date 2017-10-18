7286b9d7c3a27f4e8e1db7b27ff71e316b7d368e
Yash Mehrotra <yashmehrotra95@gmail.com>
2017-10-18 10:30:41 +0530

    Fix pandey's mistakes

--- predict.py
+++ predict.py
@@ -4,6 +4,7 @@ import cgi
 import cgitb
 from nltk.corpus import brown
 from pytrie import SortedStringTrie as Trie
+from __future__ import print_function
 
 
 test_sentence = "has been"
@@ -11,17 +12,18 @@ prefix_tries = Trie()
 sentences = brown.sents()
 # print len(sentences)
 word_index = 0
-for sent in sentences:
-	merged_sentence = " ".join(sent)
-	# print merged_sentence
-	if test_sentence in merged_sentence:
-		filtered_sentence = merged_sentence.split(test_sentence)
-		filtered1_sentence = filtered_sentence[1:]
-		for iter_sentence in filtered1_sentence:
-			filtered2_sentence = re.sub(r'[^\w\s]','',iter_sentence)
-			filtered2_sentence = filtered2_sentence.strip()
-			predicted_menuitem = filtered2_sentence.partition(' ')[0]
-			# print predicted_menuitem
-			prefix_tries[predicted_menuitem.lower()] = word_index
-			word_index = word_index + 1
-print prefix_tries.items(prefix='a')
+for word in sentences:
+    merged_sentence = " ".join(word)
+    # print merged_sentence
+    if test_sentence in merged_sentence:
+        filtered_sentence = merged_sentence.split(test_sentence)
+        filtered1_sentence = filtered_sentence[1:]
+        for iter_sentence in filtered1_sentence:
+            filtered2_sentence = re.sub(r'[^\w\s]','',iter_sentence)
+            filtered2_sentence = filtered2_sentence.strip()
+            predicted_menuitem = filtered2_sentence.partition(' ')[0]
+            # print predicted_menuitem
+            prefix_tries[predicted_menuitem.lower()] = word_index
+            word_index = word_index + 1
+
+print(prefix_tries.items(prefix='a'))
